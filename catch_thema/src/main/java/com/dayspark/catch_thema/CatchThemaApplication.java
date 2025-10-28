0e08a28402ac680f4a4a5e43d60b6da62cbe728f
KKdohyeong <dohyeong254@naver.com>
2025-10-28 11:13:01 +0900

    init: 스프링 부트 초기 세팅

---
+++ catch_thema/src/main/java/com/dayspark/catch_thema/CatchThemaApplication.java
@@ -0,0 +1,13 @@
+package com.dayspark.catch_thema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchThemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatchThemaApplication.class, args);
+    }
+
+}
---
+++ catch_thema/src/test/java/com/dayspark/catch_thema/CatchThemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dayspark.catch_thema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchThemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
